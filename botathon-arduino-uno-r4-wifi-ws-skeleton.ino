f042baa7e075a76af03ed450fafb86e3e91af4d7
Sebastian King <sebastian-king@users.noreply.github.com>
2024-03-20 22:26:05 -0500

    add reconnect ability

--- botathon-arduino-uno-r4-wifi-ws-skeleton.ino
+++ botathon-arduino-uno-r4-wifi-ws-skeleton.ino
@@ -9,7 +9,12 @@ WebSocketClient client;
 const char* ssid = "UNT Robotics Botathon";
 const char* password = "untrobotics";
 
-int TEAM_NO = 100;
+const char* websocket_host = "untrobotics.com";
+const int websocket_port = 81;
+const char* websocket_path = "/";
+const char* websocket_protocol = "team";
+
+int TEAM_NUM = 100;
 
 int total_time = 0;
 
@@ -29,16 +34,26 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  Serial.print("You are team #");
+  Serial.println(TEAM_NUM);
+
   client.onOpen([](WebSocket &ws) {
     Serial.println("WEBSOCKET_CONNECTED");
     // send message to server when connected
     char s[14];
-    sprintf(s, "ESP32_TEAM_%d", TEAM_NO);
+    sprintf(s, "ESP32_TEAM_%d", TEAM_NUM);
     ws.send(WebSocket::DataType::TEXT, s, strlen(s));
   });
   client.onClose([](WebSocket &ws, const WebSocket::CloseCode code,
                    const char *reason, uint16_t length) {
     Serial.println("WEBSOCKET_DISCONNECTED");
+    client.terminate();
+
+    Serial.println("Attempting to reconnect...");
+    while (!client.open(websocket_host, websocket_port, websocket_path, websocket_protocol)) {
+      Serial.println(F("WebSocket connection failed!"));
+      delay(2000);
+    }
   });
   client.onMessage([](WebSocket &ws, const WebSocket::DataType dataType,
                      const char *message, uint16_t length) {
@@ -46,10 +61,9 @@ void setup() {
     Serial.println(message);
   });
 
-  bool opened = client.open("untrobotics.com", 81, "/", "team");
-
-  if (!opened) {
-    Serial.println(F("Connection failed! Please restart the Arduino."));
+  while (!client.open(websocket_host, websocket_port, websocket_path, websocket_protocol)) {
+    Serial.println(F("WebSocket connection failed!"));
+    delay(2000);
   }
 }
 
